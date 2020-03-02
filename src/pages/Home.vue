a1ec7d4acb5ff562ffb3e7e9d1227044b1e031fc
Kukks <evilkukka@gmail.com>
2020-03-02 20:05:03 +0100

    fix bottom toolbar

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,11 +5,79 @@
             <p>{{$device}} </p>
         </f7-block>
         <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+        </f7-block>
+        <f7-block>
+            <p>{{$theme}} </p>
+            <p>{{$device}} </p>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+            <f7-block>
+                <p>{{$theme}} </p>
+                <p>{{$device}} </p>
+            </f7-block>
+        </f7-block>
+        <f7-block>
             <f7-segmented>
 
                 <f7-button :href="generateUrl(Routes.AddServer)">Add Server</f7-button>
             </f7-segmented>
         </f7-block>
+        <f7-block>
+            <p>END</p>
+        </f7-block>
+       
+        <f7-toolbar>
+            <f7-link>Lesasadasft Link</f7-link>
+            <f7-link>Right Link</f7-link>
+        </f7-toolbar>
+        <f7-toolbar bottom>
+            <f7-link>Left Link</f7-link>
+            <f7-link>Right Link</f7-link>
+        </f7-toolbar>
     </f7-page>
 </template>
 <script lang="ts">
